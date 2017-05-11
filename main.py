f4d58084c8aa532fe2a29eb2ce6f9eaa6112969d
Ximura <orukin@gmail.com>
2017-05-11 20:44:10 +0300

    update

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+
 class Dimension(object):
     def __init__(self, text: str):
         dimensions = [int(s) for s in text.split(' ')]
@@ -17,9 +20,22 @@ class Matrix(object):
         format = '\n'.join([row.__str__() for row in self.matrix])
         return format
 
+    ''' 
+     | <- ^
+     v -> |
+    '''
     def rotate(self, dimension: Dimension):
+        new_matrix = deepcopy(self.matrix)
+        for i in range(dimension.rows):
+            for j in range(dimension.cols):
+                if j > i and j > 0:  # moving leftward
+                    new_matrix[i][j-1] = self.matrix[i][j]
+                elif j <= i < dimension.rows - 1:  # moving downward
+                    new_matrix[i+1][j] = self.matrix[i][j]
+                elif 0 < i <= j:  # moving upward
+                    new_matrix[i-1][j] = self.matrix[i][j]
 
-        pass
+        self.matrix = new_matrix
 
 
 def load_input(file_path: str) -> (str, str):
@@ -35,7 +51,11 @@ def parse_input(file_path: str) -> (str, str):
 
 
 if __name__ == '__main__':
-    filepath = "input"
+    filepath = "input-1"
     dimension, matrix = parse_input(filepath)
     print(dimension)
+    print(matrix)
+
+    matrix.rotate(dimension)
+    print("---------------")
     print(matrix)
